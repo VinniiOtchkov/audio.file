045a65489d3167995477531fd44a9e8d004e3957
vinniiotchkov <vinniiotchkov@gmail.com>
2017-08-30 13:43:22 -0700

    Comments displaying on page with click

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,18 +1,14 @@
 <template>
 <div class="post">
 
-  <v-carousel-item src="/">
     <v-card id="card">
       <v-card-media>
-        <iframe width="auto" height="auto" scrolling="no" frameborder="no" :src="posts.sample_url">
-      </iframe>
-
       </v-card-media>
       <v-card-title primary-title>
         <div>
-          <h3 class="headline mb-0">{{posts.sample_artist}}</h3>
-          <h4 class="headline mb-0">{{posts.sample_title}}</h4>
-          <div>{{posts.sample_description}}</div>
+          <h3 class="headline mb-0">{{post.sample_artist}}</h3>
+          <h4 class="headline mb-0">{{post.sample_title}}</h4>
+          <div>{{post.sample_description}}</div>
         </div>
       </v-card-title>
       <v-card-actions>
@@ -22,16 +18,15 @@
         <span>-Submitted by Vinnii Otchkov</span>
       </v-card-actions>
     </v-card>
-  </v-carousel-item>
 
 </div>
 </template>
 
 <script>
-console.log('this', this.props)
+console.log('this', this)
 export default {
   name: 'post',
-  props: ['posts'],
+  props: ['post'],
 
   data() {
     return {}
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,22 +1,62 @@
 <template>
-  <v-carousel interval=6000000>
-    <Post v-for="posts in posts" :posts="posts" />
-  </v-carousel>
+<v-carousel :interval=6000000>
+
+  <v-carousel-item v-for="post in posts" src="/">
+    <v-card id="card">
+      <v-card-media>
+        {{post.sample_url}}
+      </v-card-media>
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">{{post.sample_artist}}</h3>
+          <h4 class="headline mb-0">{{post.sample_title}}</h4>
+          <div>{{post.sample_description}}</div>
+        </div>
+      </v-card-title>
+      <v-card-actions>
+
+        <v-btn icon v-on:click="showComments=!showComments">
+          <v-icon>comment</v-icon>
+        </v-btn>
+        <span>-Submitted by Vinnii Otchkov</span>
+      </v-card-actions>
+    </v-card>
+
+    <v-card id="card" v-for="post in posts" v-if="showComments">
+      <v-card-media>
+        {{post.beat_url}}
+      </v-card-media>
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">{{post.producer_name}}</h3>
+          <h4 class="headline mb-0">{{post.beat_title}}</h4>
+          <div>{{post.beat_description}}</div>
+        </div>
+      </v-card-title>
+      <v-card-actions>
+        <v-btn icon>
+          <v-icon v-on:click="">favorite</v-icon>
+        </v-btn>
+        <span>-Submitted by Vinnii Otchkov</span>
+      </v-card-actions>
+    </v-card>
+
+  </v-carousel-item>
+
+</v-carousel>
 </template>
 
 
 <script>
-import Post from './Post.vue';
-
 console.log('LOOK AT ME', this);
 
 export default {
   name: 'post-list',
-  components: {
-    Post
-  },
+  components: {},
   data() {
-    return {}
+    return {
+      showComments: false
+    }
   },
 
   computed: {
